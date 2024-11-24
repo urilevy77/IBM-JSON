cbcaaa2aecba64328aac7bb94225c052a0cdb441
Uri Levy <urilevy1999@gmail.com>
2024-11-24 15:47:21 +0200

    changed prompt for json

--- master.py
+++ master.py
@@ -1,10 +1,13 @@
 from huggingface_hub import InferenceClient
+
+
 def writeToFile(path, prompt):
     file = open(path, 'w')
     file.write(prompt)
     file.close()
 
-def read_file_sequentially(file_path,theme_path, prompt):
+
+def read_file_sequentially(file_path, theme_path, prompt):
     with open(file_path, 'r') as file:
         for line in file:
             structure = line.strip()
@@ -12,7 +15,7 @@ def read_file_sequentially(file_path,theme_path, prompt):
                 for item in theme_file:
                     theme = item.strip()
                     second_response = client.text_generation(
-                        prompt=structure+prompt+theme,
+                        prompt=structure + prompt + theme,
                         model="mistralai/Mixtral-8x7B-Instruct-v0.1",
                         temperature=0.8,
                         max_new_tokens=500,
@@ -21,26 +24,31 @@ def read_file_sequentially(file_path,theme_path, prompt):
                     )
                     print(second_response)
 
+
 def generate_json(schema_path, prompt):
     with open(schema_path, 'r') as file:
         content = file.read()
         response = client.text_generation(
-            prompt=prompt +" "+ content,
+            prompt=prompt + " " + content,
             model="mistralai/Mixtral-8x7B-Instruct-v0.1",
             temperature=0.8,
             max_new_tokens=500,
-            seed=42,
+            seed=44,
             return_full_text=False,
         )
         print(response)
 
+
 if __name__ == "__main__":
     client = InferenceClient()
-    #second_prompt = """ Generate JSON Schema using the above Structure format and the following theme: """
-    prompt = "You are an AI designed to generate JSON instances based on a provided JSON schema. The schema defines the structure, types, and constraints for JSON objects. Using the following schema, create multiple valid JSON instances that follow the rules specified. Ensure the JSON instances are diverse and cover different variations allowed by the schema. "
+    # second_prompt = """ Generate JSON Schema using the above Structure format and the following theme: """
+    json_prompt = "You are an AI designed to generate JSON instances based on a provided JSON schema. The schema defines " \
+             "the structure, types, and constraints for JSON objects. Using the following schema, create valid " \
+             "JSON instances that follow the rules specified. Ensure the JSON instances are diverse and cover " \
+             "different variations allowed by the schema. "
     generate_json("JSONschema", prompt)
 
-    #read_file_sequentially("storyStructures","themes",second_prompt)
+    # read_file_sequentially("storyStructures","themes",second_prompt)
     # system = """ "story" can be defined as a structured representation of information that outlines a specific theme or subject matter. each "story" consists set of attributes that provide detailed data points related to that theme. The attributes serve to describe various aspects of the story, allowing for a comprehensive understanding of the subject.
     # """
     # first_response = client.text_generation(
@@ -65,4 +73,3 @@ if __name__ == "__main__":
     #     return_full_text=False,
     # )
     # print(first_response)
-
